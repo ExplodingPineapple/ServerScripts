3cf45d66b6705271e14a3cfdca2543aa9f92c6d7
Dean Austin <deanosim1@explodingpineapple.com>
2016-05-05 04:17:46 +1000

    Remove second webmin mirror

--- Ubuntu/InstallWebmin-Ubuntu.sh
+++ Ubuntu/InstallWebmin-Ubuntu.sh
@@ -1,7 +1,7 @@
 # Ubuntu Webmin install script v0.1
 # created by Deanosim1
 echo 'deb http://download.webmin.com/download/repository sarge contrib
-deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib' > /tmp/webmin.list
+#deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib' > /tmp/webmin.list
 sudo cp /tmp/webmin.list /etc/apt/sources.list.d/
 echo 'copied webmin.list'
 rm /tmp/webmin.list
